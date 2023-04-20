a47e356d995bd54e2884594e60cbed03f80fa26d
NaaAtswei <nanaofosuhemaa11@gmail.com>
2023-04-20 07:54:23 -0700

    Grandeur

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -1,15 +1,15 @@
 #include<studio.h>
 /**
- * main - Entry point
+ * main - A that prints the size of various computer types
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	printf("size of a char: %d byte(s)\n", sizeof(char));
-	printf("size of an int: %d byte(s)\n", sizeof(int));
-	printf("size of a long int: %d byte(s)\n", sizeof(long int));
-	printf("size of a long long int: %d byte(s)\n", sizeof(long long int))
-	printf("size of a float: %d byte(s)\n", sizeof(float));
+	printf("size of a char: %zu byte(s)\n", sizeof(char));
+	printf("size of an int: %zu byte(s)\n", sizeof(int));
+	printf("size of a long int: %zu byte(s)\n", sizeof(long int));
+	printf("size of a long long int: %zu byte(s)\n", sizeof(long long int))
+	printf("size of a float: %zu byte(s)\n", sizeof(float));
 	return (0);
 }
 
